b04635b7ef297df3e72af9eeca93e38bb18025cc
KOREA7777-777 <eeghi1233@gmail.com>
2026-04-03 00:55:11 +0900

    Add files via upload

--- scripts/supabase.config.js
+++ scripts/supabase.config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://(supabase name here).supabase.co';
-const SUPABASE_ANON_KEY = 'supabase anon key here';
+const SUPABASE_URL = 'https://wnkogspcwjvwulhlupml.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_dzDWW99uBpqo3UsFXm7A1g_F7zb7KPD';
 
 window.SUPABASE_URL = SUPABASE_URL;
 window.SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
